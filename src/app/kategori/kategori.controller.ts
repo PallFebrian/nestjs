b28bb27dc154d40fb4f1538de63d15ae0918f70c
PallFebrian <pallfebrian9@gmail.com>
2023-10-11 11:19:08 +0700

    tugas

--- src/app/kategori/kategori.controller.ts
+++ src/app/kategori/kategori.controller.ts
@@ -1,8 +1,24 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtGuard } from '../auth/auth.guard';
 import { KategoriService } from './kategori.service';
-import { CreateKategoriDto, findAllKategori } from './kategori.dto';
+import {
+  CreateKategoriDto,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  UpdateKategoriDto,
+  findAllKategori,
+} from './kategori.dto';
 import { Pagination } from 'src/utils/decorator/pagination.decorator';
+import { InjectCreatedBy } from 'src/utils/decorator/inject-created_by.decorator';
+import { InjectUpdatedBy } from 'src/utils/decorator/inject-updated_by.decorator';
 
 @UseGuards(JwtGuard) //  implementasikan global guard pada semua endpont kategori memerlukan authentikasi saat request
 @Controller('kategori')
@@ -10,7 +26,8 @@ export class KategoriController {
   constructor(private kategoriService: KategoriService) {}
 
   @Post('create')
-  async create(@Body() payload: CreateKategoriDto) {
+  async create(@InjectCreatedBy() payload: CreateKategoriDto) {
+    //ganti @Body() dengan @InjectCreatedBy()
     return this.kategoriService.create(payload);
   }
 
@@ -18,5 +35,24 @@ export class KategoriController {
   async getAllCategory(@Pagination() query: findAllKategori) {
     //gunakan custom decorator yang pernah kita buat
     return this.kategoriService.getAllCategory(query);
+  }
+  @Get('detail/:id')
+  getDetailKategori(@Param('id') id: string) {
+    return this.kategoriService.getDetail(Number(id));
+  }
+  @Put('update/:id')
+  updateKategori(
+    @Param('id') id: string,
+    @InjectUpdatedBy() UpdateKategoriDto: UpdateKategoriDto,
+  ) {
+    return this.kategoriService.updateKategori(Number(id), UpdateKategoriDto);
+  }
+  @Delete('delete/:id')
+  deleteKategori(@Param('id') id: string) {
+    return this.kategoriService.deleteKategori(+id);
+  }
+  @Post('createBulk')
+  async createBull(@Body() payload: CreateKategoriDto[]) {
+    return this.kategoriService.createBulk(payload);
   }
 }
--- src/app/kategori/kategori.service.ts
+++ src/app/kategori/kategori.service.ts
@@ -1,10 +1,20 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import BaseResponse from 'src/utils/response/base.response';
 import { Kategori } from './kategori.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Like, Repository } from 'typeorm';
 import { REQUEST } from '@nestjs/core';
-import { CreateKategoriDto, findAllKategori } from './kategori.dto';
+import {
+  CreateKategoriDto,
+  UpdateKategoriDto,
+  findAllKategori,
+} from './kategori.dto';
 import { ResponsePagination, ResponseSuccess } from 'src/interface';
 
 @Injectable()
@@ -19,17 +29,76 @@ export class KategoriService extends BaseResponse {
 
   async create(payload: CreateKategoriDto): Promise<ResponseSuccess> {
     try {
-      await this.kategoriRepository.save({
-        ...payload,
-        created_by: {
-          id: this.req.user.id,
-        },
-      });
+      //   await this.kategoriRepository.save({
+      //     ...payload,
+      //     created_by: {
+      //       id: this.req.user.id,
+      //     },
+      //   });
+      await this.kategoriRepository.save(payload); // cukup payload tanpa manipulasi object
 
       return this._success('OK', this.req.user.user_id);
     } catch {
       throw new HttpException('Ada Kesalahan', HttpStatus.UNPROCESSABLE_ENTITY);
     }
+  }
+
+  async getDetail(id: number): Promise<ResponseSuccess> {
+    const detailBook = await this.kategoriRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (detailBook === null) {
+      throw new HttpException(
+        `Buku dengan id ${id} tidak ditemukan`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return {
+      status: 'Success',
+      message: 'Detail Buku ditermukan',
+      data: detailBook,
+    };
+  }
+
+  async updateKategori(
+    id: number,
+    updateKategoriDto: UpdateKategoriDto,
+  ): Promise<ResponseSuccess> {
+    const check = await this.kategoriRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!check) throw new NotFoundException(`id ${id} tidak ditemukan`);
+
+    const update = await this.kategoriRepository.save({
+      ...updateKategoriDto,
+      id: id,
+    });
+    return {
+      status: `Success `,
+      message: 'Buku berhasil di update',
+      data: update,
+    };
+  }
+
+  async deleteKategori(id: number): Promise<ResponseSuccess> {
+    const check = await this.kategoriRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!check) throw new NotFoundException(`id ${id} tidak ditemukan`);
+    await this.kategoriRepository.delete(id);
+    return {
+      status: `Success `,
+      message: 'Berhasil menghapus kategori',
+    };
   }
 
   async getAllCategory(query: findAllKategori): Promise<ResponsePagination> {
@@ -68,5 +137,21 @@ export class KategoriService extends BaseResponse {
     });
 
     return this._pagination('OK', result, total, page, pageSize);
+  }
+
+  async createBulk(payload: CreateKategoriDto[]): Promise<ResponseSuccess> {
+    console.log(payload);
+    try {
+      const kategoris = payload.map((kategoriDto) => ({
+        ...kategoriDto,
+        created_by: { id: this.req.user.id },
+      }));
+
+      await this.kategoriRepository.save(kategoris);
+
+      return this._success('Bulk create successful', payload);
+    } catch (error) {
+      throw new HttpException('Ada Kesalahan', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 }
